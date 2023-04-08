de35f8cffb7c5464a20b03b82ff5c9cfdea61962
andres gomez <andres_felipe1822@hotmail.com>
2023-04-08 15:42:42 -0500

    v2.0.0

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,4 @@
-//tarea: agregar boton "C", para borrar
-// que pasa cuando sale un error?  --try catch--
-
-
-var botones=["7","8","9","+","4","5","6","-","1","2","3","*","0",".","=","/"];
+var botones=["C",".","%","/","7","8","9","*","4","5","6","-","1","2","3","+","0",".","="];
 
 function calculadora(){
     //1. Crear elemento
@@ -15,69 +11,66 @@ function calculadora(){
     divCalculadora.setAttribute("id","calculadora");
     document.body.appendChild(divCalculadora);
    
-
     //div pantalla   
     const  divPantalla= document.createElement("div");
     divPantalla.setAttribute("id","divPantalla");
-
     divCalculadora.appendChild(divPantalla);
 
-    //pantalla
+    //Input pantalla
     const pantalla= document.createElement("input");
     pantalla.setAttribute("id","pantalla");
     pantalla.setAttribute("type","text");
     pantalla.setAttribute("name","pantalla");
-    pantalla.setAttribute("value","0");
-    pantalla.setAttribute("class","border-light")
-    pantalla.setAttribute("class","")
-    //pantalla.setAttribute("class","position-absolute top-50 start-50 translate-middle")
+    pantalla.setAttribute("value"," ");
     pantalla.setAttribute("disabled","true");
-    
     divPantalla.appendChild(pantalla);
-
 
     //div botones
     const  divBotones= document.createElement("div")
     divBotones.setAttribute("id","botones");
-   
+    divBotones.setAttribute("type","button");
+    divBotones.setAttribute("class","botones");
+    divBotones.setAttribute("type","button");
     divCalculadora.appendChild(divBotones);
     
     
     for(let i=0;i<botones.length;i++){
         if(i%4==0){
             const divFila=document.createElement("div");
-            divFila.setAttribute("class","row");
+            divFila.setAttribute("class","row"+[i]);
             divBotones.appendChild(divFila);
         }
+
         let boton=document.createElement("button");
-        boton.setAttribute("id","boton"+botones[i]);
-        boton.setAttribute("class","btn btn-success col-3 border-light" );
+        boton.setAttribute("id","boton");
+        boton.setAttribute("class","btn btn-dark rounded-circle mx-1 my-1");
         boton.innerHTML=botones[i];
-      
+
         boton.addEventListener("click",function(){
             procesarEvento(boton);
-
-        })
-
+        });
         divBotones.appendChild(boton);
     }
-    
-    
-    
-   
-    
 }
+
 function procesarEvento(boton){
+
     let miPantalla=document.getElementById("pantalla");
 
-    if(miPantalla.value=="0"){
-        miPantalla.value="";
-    }
-    if(boton.innerHTML!="="){
-    miPantalla.value+=boton.innerHTML;}
-    else{
-        let resultado= math.evaluate(miPantalla.value)
-        miPantalla.value=resultado
+    try{
+        if(boton.innerHTML!="="){
+            miPantalla.value+=boton.innerHTML;
+        }
+        if(boton.innerHTML=="C"){
+            miPantalla.value=" ";
+        }
+    
+        if(boton.innerHTML=="="){
+            let resultado= math.evaluate(miPantalla.value);
+            miPantalla.value=resultado;
+        }
+    } catch(error){
+        miPantalla.value="Error";
     }
 }
 
